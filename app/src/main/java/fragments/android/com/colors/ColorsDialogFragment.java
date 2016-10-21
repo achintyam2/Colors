5fee25f4ee9a37a1138a1b85cf92b6569d33ccbe
Achintya <achintyam2@gmail.com>
2016-10-21 17:46:08 +0530

    Theme Selector

--- app/src/main/java/fragments/android/com/colors/ColorsDialogFragment.java
+++ app/src/main/java/fragments/android/com/colors/ColorsDialogFragment.java
@@ -8,38 +8,49 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
-/**
- * Created by Achintya on 20-10-2016.
- */
-public class ColorsDialogFragment extends DialogFragment implements View.OnClickListener  {
-
+public class ColorsDialogFragment extends DialogFragment implements View.OnClickListener {
     ImageView blue, green, red, orange,peach,yellow,cyan,violet;
+    int blueContext,greenContext,redContext,orangeContext,yellowContext,peachContext,cyanContext,violetContext;
+    ImageView previousSelection;
+    int presentSelection=-1;
     private static final String TAG="MyDialogFragment";
     private Context context;
+    private DialogFragmentClickHandler caller;
+    Button cancel,accept;
+
+    public ColorsDialogFragment(){}
+
+    public interface DialogFragmentClickHandler {
+        void onDialogFragmentClicked(int colorValue);
+    }
 
     public static ColorsDialogFragment newInstance() {
-        ColorsDialogFragment frag = new ColorsDialogFragment();
-        return frag;
+        ColorsDialogFragment fragment = new ColorsDialogFragment();
+        return fragment;
     }
 
     @Override
     public void onAttach(Context context){
+        super.onAttach(context);
+        caller = (DialogFragmentClickHandler) context;
         this.context=context;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-
         View rootView = inflater.inflate(R.layout.circles, container, false);
+
         blue = (ImageView) rootView.findViewById(R.id.blue);
         green = (ImageView) rootView.findViewById(R.id.green);
         red = (ImageView) rootView.findViewById(R.id.red);
@@ -48,59 +59,62 @@ public class ColorsDialogFragment extends DialogFragment implements View.OnClick
         yellow = (ImageView) rootView.findViewById(R.id.yellow);
         cyan = (ImageView) rootView.findViewById(R.id.cyan);
         violet = (ImageView) rootView.findViewById(R.id.violet);
+        cancel = (Button) rootView.findViewById(R.id.cancel);
+        accept = (Button) rootView.findViewById(R.id.accept);
+        blueContext = ContextCompat.getColor(context, R.color.blue);
+        greenContext = ContextCompat.getColor(context, R.color.green);
+        redContext = ContextCompat.getColor(context, R.color.red);
+        orangeContext = ContextCompat.getColor(context, R.color.orange);
+        peachContext = ContextCompat.getColor(context, R.color.peach);
+        yellowContext = ContextCompat.getColor(context, R.color.yellow);
+        cyanContext = ContextCompat.getColor(context, R.color.cyan);
+        violetContext = ContextCompat.getColor(context, R.color.violet);
         getDialog().setTitle("Theme Color");
+        blue.setOnClickListener(this);
+        green.setOnClickListener(this);
+        red.setOnClickListener(this);
+        peach.setOnClickListener(this);
+        yellow.setOnClickListener(this);
+        cyan.setOnClickListener(this);
+        violet.setOnClickListener(this);
+        orange.setOnClickListener(this);
+        cancel.setOnClickListener(this);
+        accept.setOnClickListener(this);
+
+
         return rootView;
     }
     @Override
-    public void onClick(View v) {
-        int id = v.getId();
-        /*ImageView selectedView = (ImageView) findViewById(id);
-        selectedView.setDrawingCacheEnabled(true);
-        int selectedColor  = selectedView.getSolidColor();
-        selectedView.setImageResource(R.drawable.ic_done_black_24dp);
-        Log.d(TAG,"Blue: "+selectedColor);*/
+    public void onClick (View v)
+    {
 
-        switch (id) {
-            case R.id.blue:
-                int blueContext = ContextCompat.getColor(context,R.color.blue);
-                blue.setImageResource(R.drawable.ic_done_black_24dp);
-                Log.d(TAG,"Blue: "+blueContext);
-                break;
-            case R.id.green:
-                int greenContext =ContextCompat.getColor(context,R.color.green);
-                Log.d(TAG,"Green: "+greenContext);
-                break;
-            case R.id.red:
-                int redContext =ContextCompat.getColor(context,R.color.red);
-                Log.d(TAG,"Red: "+redContext);
-                break;
-            case R.id.orange:
-                int orangeContext =ContextCompat.getColor(context,R.color.orange);
-                Log.d(TAG,"Orange: "+orangeContext);
-                break;
-            case R.id.peach:
-                int peachContext =ContextCompat.getColor(context,R.color.peach);
-                Log.d(TAG,"Peach: "+peachContext);
-                break;
-            case R.id.yellow:
-                int yellowContext =ContextCompat.getColor(context,R.color.yellow);
-                Log.d(TAG,"Yellow: "+yellowContext);
-                break;
-            case R.id.cyan:
-                int cyanContext =ContextCompat.getColor(context,R.color.cyan);
-                Log.d(TAG,"Cyan: "+cyanContext);
-                break;
-            case R.id.violet:
-                int violetContext =ContextCompat.getColor(context,R.color.violet);
-                Log.d(TAG,"Violet: "+violetContext);
-                break;
-            case R.id.cancel:
-
-                break;
-            case R.id.accept:
-
-                break;
+        int a=v.getId();
+        Log.d(TAG,"ID "+a);
+        if(a>= 2131427414 && a<=2131427421 ) {
+            if (previousSelection != null && v.getId() == previousSelection.getId()) {
+                ((ImageView) v).setImageResource(R.drawable.ic_done_black_24dp);
+            } else {
+                ((ImageView) v).setImageResource(R.drawable.ic_done_black_24dp);
+                presentSelection = v.getId();
+                if (previousSelection != null && presentSelection != previousSelection.getId()) {
+                    previousSelection.setImageResource(R.drawable.ic_done_blank_24dp);
+                }
+                previousSelection = (ImageView) v;
+            }
         }
+        else if(a==2131427422)
+        {
+            dismiss();
+        }
+        else
+        {
+             //ColorDrawable drawable = (ColorDrawable) previousSelection.getBackground();
+             //presentSelection=drawable.getColor();
+            MainActivity call = (MainActivity) getActivity();
+            call.onDialogFragmentClicked(presentSelection);
+            dismiss();
+        }
+
     }
 
 }
--- app/src/main/java/fragments/android/com/colors/MainActivity.java
+++ app/src/main/java/fragments/android/com/colors/MainActivity.java
@@ -1,30 +1,30 @@
 package fragments.android.com.colors;
 
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * Created by Achintya on 20-10-2016.
- */
-public class MainActivity extends FragmentActivity   {
+public class MainActivity extends FragmentActivity implements ColorsDialogFragment.DialogFragmentClickHandler   {
 
     Button button;
     private static final String TAG="MainActivity";
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+         text = (TextView)findViewById(R.id.text);
         button = (Button)findViewById(R.id.button) ;
-
-
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
                 showDialog();
+
             }
         });
     }
@@ -33,5 +33,13 @@ public class MainActivity extends FragmentActivity   {
             FragmentTransaction ft = getFragmentManager().beginTransaction();
             ColorsDialogFragment newFragment = ColorsDialogFragment.newInstance();
             newFragment.show(ft,"title");
+
         }
+
+    @Override
+    public void onDialogFragmentClicked(int colorValue) {
+        Toast.makeText(getApplicationContext(),"Value: "+colorValue,Toast.LENGTH_LONG).show();
+
+    }
+
 }
