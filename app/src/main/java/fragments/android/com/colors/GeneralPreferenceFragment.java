7b435bca4b3996eddacc9d4e67e887d392c5da81
Achintya <achintyam2@gmail.com>
2016-11-07 19:27:11 +0530

    Themes Selector ,Google Intgration, Gmail Integration ,Facebook Integration in a ViewPager Added Settings

---
+++ app/src/main/java/fragments/android/com/colors/GeneralPreferenceFragment.java
@@ -0,0 +1,42 @@
+package fragments.android.com.colors;
+
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.view.MenuItem;
+
+import static fragments.android.com.colors.SettingsActivity.bindPreferenceSummaryToValue;
+
+/**
+ * This fragment shows general preferences only. It is used when the
+ * activity is showing a two-pane settings UI.
+ */
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class GeneralPreferenceFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.pref_general);
+        setHasOptionsMenu(true);
+
+        // Bind the summaries of EditText/List/Dialog/Ringtone preferences
+        // to their values. When their values change, their summaries are
+        // updated to reflect the new value, per the Android Design
+        // guidelines.
+        bindPreferenceSummaryToValue(findPreference("example_text"));
+        bindPreferenceSummaryToValue(findPreference("example_list"));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            startActivity(new Intent(getActivity(), SettingsActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/fragments/android/com/colors/SettingsPreferenceFragment.java
@@ -0,0 +1,100 @@
+package fragments.android.com.colors;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.preference.SwitchPreference;
+import android.util.Log;
+import android.view.MenuItem;
+
+import static fragments.android.com.colors.SettingsActivity.bindPreferenceSummaryToValue;
+
+/**
+ * This fragment shows notification preferences only. It is used when the
+ * activity is showing a two-pane settings UI.
+ */
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class SettingsPreferenceFragment extends PreferenceFragment{
+
+    public SwitchPreference wifi,bluetooth;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.pref_settings);
+        setHasOptionsMenu(true);
+
+        wifi = (SwitchPreference) findPreference("example_wifi");
+        bluetooth = (SwitchPreference) findPreference("example_bluetooth");
+
+        if (wifi!=null) {
+            wifi.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    boolean test = Boolean.parseBoolean(newValue.toString());
+                    Log.d("tag", "test " + test);
+                    if (test)
+                        wifi.setSummary("Enabled");
+                    else
+                        wifi.setSummary("Disabled");
+
+                    return true;
+                }
+            });
+        }
+            if (bluetooth!=null) {
+                bluetooth.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                    @Override
+                    public boolean onPreferenceChange(Preference preference, Object newValue) {
+                        boolean test = Boolean.parseBoolean(newValue.toString());
+                        Log.d("tag", "test " + test);
+                        if (test)
+                            bluetooth.setSummary("Enabled");
+                        else
+                            bluetooth.setSummary("Disabled");
+
+                        return true;
+                    }
+                });
+        }
+            // Bind the summaries of EditText/List/Dialog/Ringtone preferences
+            // to their values. When their values change, their summaries are
+            // updated to reflect the new value, per the Android Design
+            // guidelines.
+            bindPreferenceSummaryToValue(findPreference("example_settings_text"));
+            bindPreferenceSummaryToValue(findPreference("example_settings_list"));
+        }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            startActivity(new Intent(getActivity(), SettingsActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    SharedPreferences.OnSharedPreferenceChangeListener listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            if (key.equals("example_wifi")) {
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+                boolean test = preferences.getBoolean("example_wifi", false);
+                Log.d("h", "test = " + test);
+                if (test)
+                    wifi.setSummary("Enabled");
+                else
+                    wifi.setSummary("Disabled");
+            }
+        }
+    };
+}
+
+
